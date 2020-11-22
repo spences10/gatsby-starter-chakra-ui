681966066acafee357a1e0e69676fa6b219ecba7
spences10 <spences10apps@gmail.com>
2020-11-22 11:16:27 +0000

    author and title change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Starter Chakra UI`,
+    description: `Kick off your next, great Gatsby project with this Chakra UI starter.`,
+    author: `@spences10`,
   },
   plugins: [
     `@chakra-ui/gatsby-plugin`,
