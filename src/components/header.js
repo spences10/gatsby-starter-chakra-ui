a8653a425637d49f97ab765d94c90598d7a10e5e
spences10 <spences10apps@gmail.com>
2020-11-30 09:09:01 +0000

    fix toggle postion

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import { Box, Heading, Link } from '@chakra-ui/react'
+import { Box, Flex, Heading, Link, Spacer } from '@chakra-ui/react'
 import { Link as GatsbyLink } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -7,19 +7,22 @@ import ThemeToggle from './theme-toggle'
 const Header = ({ siteTitle }) => (
   <Box as="header" background="rebeccapurple" marginBottom="1.45rem">
     <Box as="div" m="0 auto" maxW="960px" p="1.45rem 1.0875rem">
-      <Heading margin="0">
-        <Link
-          as={GatsbyLink}
-          to="/"
-          color="white"
-          _hover={{ textDecor: 'none' }}
-        >
-          {siteTitle}
-        </Link>
-      </Heading>
-    </Box>
-    <Box as="div" position="fixed" right="20px" top="20px">
-      <ThemeToggle />
+      <Flex>
+        <Heading margin="0">
+          <Link
+            as={GatsbyLink}
+            to="/"
+            color="white"
+            _hover={{ textDecor: 'none' }}
+          >
+            {siteTitle}
+          </Link>
+        </Heading>
+        <Spacer />
+        <Box as="div" position="relative">
+          <ThemeToggle />
+        </Box>
+      </Flex>
     </Box>
   </Box>
 )
