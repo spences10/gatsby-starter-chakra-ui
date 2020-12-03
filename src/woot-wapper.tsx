f1c7f8e0a1408387fa5490d96166a2a005727650
HAINES-DESKTOP\richa <hello@richardhaines.dev>
2020-12-03 11:02:04 +0100

    Added theme file

--- src/woot-wapper.tsx
+++ src/woot-wapper.tsx
@@ -1,10 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import React from "react"
-import Layout from "./components/layout"
+import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react'
+import Layout from './components/layout'
+import { theme } from './theme'
 
 export const wrapPageElement = ({ element }) => {
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider theme={theme} resetCSS>
       <Layout>{element}</Layout>
     </ChakraProvider>
   )
